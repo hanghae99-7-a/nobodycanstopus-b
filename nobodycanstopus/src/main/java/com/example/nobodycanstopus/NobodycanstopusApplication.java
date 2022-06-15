043caea9829272d45ad9f0ff898a2cca7dcd53f6
mjoonho <103565908+mjoonho@users.noreply.github.com>
2022-06-15 14:53:01 +0900

    h2콘솔 연결

--- nobodycanstopus/src/main/java/com/example/nobodycanstopus/NobodycanstopusApplication.java
+++ nobodycanstopus/src/main/java/com/example/nobodycanstopus/NobodycanstopusApplication.java
@@ -2,8 +2,10 @@ package com.example.nobodycanstopus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NobodycanstopusApplication {
 
     public static void main(String[] args) {
